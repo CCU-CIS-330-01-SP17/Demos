8327d9a0e4d79580cab338ae244027ae019067d9
Matt Anderson <mattanderson@ga.ccu.edu>
2017-01-02 16:29:09 -0700

    Added OOPPillars solution.

---
+++ OOPPillars/OOPPillars/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPillars
+{
+    public abstract class Animal
+    {
+        private int numberOfLegs;
+
+        public int NumberOfLegs
+        {
+            get
+            {
+                return numberOfLegs;
+            }
+            set
+            {
+                if (value > 8)
+                {
+                    throw new ArgumentException("Number of legs must be less than or equal to 8.");
+                }
+
+                numberOfLegs = value;
+            }
+        }
+
+        public abstract void Speak();
+    }
+}
---
+++ OOPPillars/OOPPillars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPillars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Giraffe g = new Giraffe();
+            g.NumberOfLegs = 4;
+            g.Speak();
+            Console.WriteLine(g.NumberOfLegs);
+
+            Animal a = GetAnimalFromBarn();
+            a.Speak();
+        }
+
+        static Animal GetAnimalFromBarn()
+        {
+            return new Giraffe();
+        }
+    }
+}
