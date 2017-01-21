7ad916d78e3dabd45c5d8edd71102fc51b91212d
Matt Anderson <mattanderson@ga.ccu.edu>
2017-01-20 17:50:23 -0700

    Added OOPDemo solution from in-class discussion of OOP pillars.

---
+++ OOPDemo/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    class Animal
+    {
+        private int height;
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Yum yum!");
+        }
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Animal();
+            //a.Height = 23;
+
+            Console.WriteLine(a.Height);
+
+            Animal d = new Dog();
+            d.Eat();
+        }
+    }
+}
