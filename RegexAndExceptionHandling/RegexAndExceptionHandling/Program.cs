1025d13e571f78fe7a19b84ac2c21a51a057c6db
Matt Anderson <mattanderson@ga.ccu.edu>
2017-02-13 19:45:38 -0700

    Added Regex and Exception Handling demos solution. Added Week 5 Exam Prep questions.

---
+++ RegexAndExceptionHandling/RegexAndExceptionHandling/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexAndExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Regex.Match("color", @"colou?r").Success);
+
+            Console.WriteLine(Regex.Match("say 25 miles more", @"\d+(?=\smiles)"));
+
+            Console.WriteLine(Regex.Match("say 25 miles more", @"(?<=say\s)\d+"));
+
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            //Console.WriteLine(ExceptionHandlingDemo());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("**** My Custom Unhandled Exception Handler *****: {0}", e.ExceptionObject.ToString());
+        }
+
+        private static int? ExceptionHandlingDemo()
+        {
+            string x = "4";
+            string y = "2";
+
+            try
+            {
+                return DivideStringValues(x, y);
+            }
+            catch (ArgumentNullOrWhitespaceException ex)
+            {
+                Console.WriteLine("{0}: {1}", ex.GetType().Name, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("{0}: {1}", ex.GetType().Name, ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("{0}: {1}", ex.GetType().Name, ex.Message);
+                //return -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("*** WARNING: Fallback Handler Used ***: {0}: {1}", ex.GetType().Name, ex.Message);
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+            return null;
+        }
+
+        private static int DivideStringValues(string x, string y)
+        {
+            if (string.IsNullOrWhiteSpace(x))
+            {
+                throw new ArgumentNullOrWhitespaceException("Argument must have a value other than null or whitespace.", nameof(x));
+            }
+
+            if (string.IsNullOrWhiteSpace(y))
+            {
+                throw new ArgumentNullOrWhitespaceException("Argument must have a value other than null or whitespace.", nameof(y));
+            }
+
+            int xValue = Int32.Parse(x);
+            int yValue = Int32.Parse(y);
+            //int yValue;
+
+            if (!Int32.TryParse(y, out yValue))
+            {
+                throw new ArgumentException($"The value \"{y}\" for y could not be converted to an int.");
+            }
+
+            return xValue / yValue;
+        }
+    }
+}
---
+++ Week5ExamPrep/Week5ExamPrep/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5ExamPrep
+{
+    class Program
+    {
+        class Animal { }
+        class Frog : Animal { }
+        static void Speak(object obj) { Console.WriteLine("In Speak(object)"); }
+        static void Speak(Animal a) { Console.WriteLine("In Speak(Animal)"); }
+        static void Speak(Frog f) { Console.WriteLine("In Speak(Frog)"); }
+
+        static void Main(string[] args)
+        {
+            object f = new Frog();
+            Speak(f);
+
+            BaseLogger logger = new Logger();
+            logger.Log("Log started");
+            logger.Log("Base: Log continuing");
+            ((Logger)logger).LogCompleted();
+        }
+    }
+
+    public abstract class BaseLogger
+    {
+        public virtual void Log(string message)
+        {
+            Console.WriteLine("Base: " + message);
+        }
+
+        public void LogCompleted()
+        {
+            Console.WriteLine("Completed");
+        }
+    }
+
+    public class Logger : BaseLogger
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public new void LogCompleted()
+        {
+            Console.WriteLine("Finished");
+        }
+    }
+
+}
